fe1b87bbbe6c6f383078131c132bf15bb5bddaba
Denis Dmitriev <eto.pochta.deniski@gmail.com>
2022-05-15 09:36:30 +0300

    Create repository

---
+++ js/main.js
@@ -0,0 +1,63 @@
+import '/js/scroll-timeline.js';
+
+$(document).ready(function () {
+	// Scroll Events
+	$(window).scroll(function () {
+
+		var wScroll = $(this).scrollTop();
+
+		// Activate menu
+		if (wScroll > 20) {
+			$('#main-nav').addClass('active');
+			$('#menu-close').addClass('active');
+		}
+		else {
+			$('#main-nav').removeClass('active');
+			$('#menu-close').removeClass('active');
+		};
+
+		//Scroll Effects
+
+	});
+
+	// Navigation
+	$('#navigation').on('click', function (e) {
+		e.preventDefault();
+		$('#body').toggleClass('open');
+		$('#menu').toggleClass('open');
+		$('#progress').toggleClass('open');
+
+		if ($('#menu').hasClass('open')) {
+			$('.menu-close').on('click', function (e) {
+				e.preventDefault();
+				$('#menu').removeClass('open');
+				$('#body').removeClass('open');
+				$('#progress').removeClass('open');
+			})
+		}
+	});
+
+	//Progress Bar
+	// Create ScrollTimeline
+	const myScrollTimeline = new ScrollTimeline({
+		source: document.scrollingElement,
+		scrollSource: document.scrollingElement, // For legacy implementations
+		orientation: 'block',
+		scrollOffsets: [
+			new CSSUnitValue(0, 'percent'),
+			new CSSUnitValue(100, 'percent'),
+		],
+	});
+	// Animate Progress Bar on Scroll
+	new Animation(
+		new KeyframeEffect(
+			document.querySelector('#progress'),
+			{
+				transform: ['scaleX(0)', 'scaleX(1)'],
+			},
+			{ duration: 1, fill: "forwards" }
+		),
+		myScrollTimeline
+	).play();
+
+});
